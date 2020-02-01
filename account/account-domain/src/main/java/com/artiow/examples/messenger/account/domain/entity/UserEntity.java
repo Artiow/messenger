262f78b63e493c67fdab00525224d427f4849e1c
artiow <namednev.artem@gmail.com>
2020-02-01 18:55:20 +0300

    ELK logging support add, package refactoring

--- account/account-domain/src/main/java/com/artiow/examples/messenger/account/domain/entity/UserEntity.java
+++ account/account-domain/src/main/java/com/artiow/examples/messenger/account/domain/entity/UserEntity.java
@@ -1,13 +1,17 @@
 package com.artiow.examples.messenger.account.domain.entity;
 
 import com.artiow.examples.messenger.domain.entity.AbstractAuditableEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Entity
+@Getter
+@Setter
 @Table(name = "user")
+@Entity(name = "user")
 public class UserEntity extends AbstractAuditableEntity {
 
     @Basic(optional = false)
