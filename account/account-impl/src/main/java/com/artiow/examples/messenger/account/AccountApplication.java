a5489b597042d792abbe478253510bb7f48bf16d
artiow <namednev.artem@gmail.com>
2020-02-04 19:58:40 +0300

    Zuul gateway support add

--- account/account-impl/src/main/java/com/artiow/examples/messenger/account/AccountApplication.java
+++ account/account-impl/src/main/java/com/artiow/examples/messenger/account/AccountApplication.java
@@ -2,12 +2,14 @@ package com.artiow.examples.messenger.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJpaAuditing
 @EnableJpaRepositories
+@EnableDiscoveryClient
 @EnableTransactionManagement
 @SpringBootApplication
 public class AccountApplication {
